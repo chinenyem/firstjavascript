4aed6d28ebca59c6ea7ac58b65c4aba7b50ec24e
chinenyem nwadiugwu <chinenyemnwadiugwu@chinenyems-MacBook-Air.local>
2014-07-31 09:56:15 -0400

    first commit

---
+++ introductionjavascript.js
@@ -0,0 +1,140 @@
+
+
+// numbers //
+
+// adding the datatype number//
+80 + 80   
+
+// adding the booleans and number//
+true + 1
+
+// adding number and string//
+
+5 + "foo"
+
+// strings //
+
+// concatenates strings together //
+"love" + "hurt" + "hate" + "food"
+
+// strings are true for they are the same character and spaces"
+
+"Patch my boat with chewing gum." == "Patch my boat with chewing gum."
+
+// string turned into an array //
+"chinenyemlovesfrankeandrueben" .split("")
+
+//array//
+
+// length of the array//
+["c", "h", "i", "n", "e", "n", "y", "e", "m", "l", "o", "v", "e", "s", "f", "r", "a", "n", "k", "e", "a", "n", "d", "r", "u", "e", "b", "e", "n"] .length
+
+// reverse the array including a string //
+[4, "love", 8] .reverse ("")
+
+//push array//
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+undefined
+fruits.push("Kiwi");
+5
+fruits
+["Banana", "Orange", "Apple", "Mango", "Kiwi"]
+
+// function
+
+var arr = [];
+arr[0] = "Jani";
+arr[1] = "Hege";
+arr[2] = "Stale";
+arr[3] = "Kai Jim";
+arr[4] = "Borge";
+"Borge"
+var arr = [];
+arr[0] = "Jani";
+arr[1] = "Hege";
+arr[2] = "Stale";
+arr[3] = "Kai Jim";
+arr[4] = "Borge";
+
+console.log(arr.join());
+arr.splice(2, 0, "Lene");
+console.log(arr.join());
+Jani,Hege,Stale,Kai Jim,Borge VM2264:9
+Jani,Hege,Lene,Stale,Kai Jim,Borge VM2264:11
+undefined
+
+
+function chinenyem = function(x, y)
+
+function chinenyem(x, y) {
+	return "howard"
+}
+
+
+
+function clickHandler() {
+     alert("Hello, World!");
+  }
+   return clickHandler
+
+// object //
+
+// this is an example of a mutation. turning propertyName into length
+// and text into mainline//
+
+var propertyName = "length";
+var text = "mainline";
+
+propertyName + text 
+
+//booleans
+
+[ 2,4,8, 6] > [1,1,1,1]
+true
+
+var love = 5
+undefined
+var hate = 6
+undefined
+love < hate
+true
+
+var love = 5
+undefined
+var hate = 6
+undefined
+love < hate
+true
+var cat =10
+undefined
+var car =1
+undefined
+car + cat === love + hate
+true
+
+var chinedu = 500;
+undefined
+chinedu = chinedu - 100;
+console .log(chinedu);
+undefined
+400
+
+
+
+var mouse = function(b){
+       
+	return b * 19;
+};
+ mouse(2);
+
+
+function house(a,b,c) {
+     var bedroom = (a);
+ 	 var office = (b);
+ 	 var kitchen = (c);
+ 	 var buildhouse = bedroom + office + kitchen;
+
+       return buildhouse;
+ }
+
+  house('mom room,' , 'mom office' , 'mom kitchen');
